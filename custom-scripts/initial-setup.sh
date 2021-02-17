0702ed535265aec1ccadc6d0d8d77c4e17146c19
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-17 18:27:08 +0000

    move file locations

--- custom-scripts/initial-setup.sh
+++ custom-scripts/initial-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # appends custom config to pacman.conf file
-curl -s https://raw.githubusercontent.com/johnobla/dot-files/main/custom_pacman.conf | tee -a /etc/pacman.conf >/dev/null
+curl -s https://raw.githubusercontent.com/johnobla/dot-files/main/custom-pacman.conf | tee -a /etc/pacman.conf >/dev/null
 
 # refresh pacman gpg keys
 pacman-key --init
@@ -11,9 +11,6 @@ pacman -Sy archlinux-keyring
 
 # update all packages to latest versions
 pacman -Syyu
-
-# install zsh
-pacman -S --noconfirm zsh
 
 # add sudo group
 groupadd sudo

32806ddc54739b70d536179f63a92cedea8e0ae6
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-18 14:05:00 +0000

    add fish install details

--- custom-scripts/oh-my-fish-install.sh
+++ custom-scripts/oh-my-fish-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# add fish to shells
+# echo /usr/local/bin/fish | sudo tee -a /etc/shells
+
+# change default shell to fish
+# chsh -s /usr/local/bin/fish
+
 # install oh my fish
-# uncomment below when ready
 # curl -L https://get.oh-my.fish | fish

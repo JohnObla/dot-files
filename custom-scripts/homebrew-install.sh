4e9950203cfbabb45b95177c3f824c1757ea2bfb
John Obla <45395888+johnobla@users.noreply.github.com>
2021-02-18 13:40:34 +0000

    make install username agnostic

--- custom-scripts/homebrew-install.sh
+++ custom-scripts/homebrew-install.sh
@@ -4,8 +4,10 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add homebrew to path
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/john/.profile
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
 # add homebrew dependencies
 sudo apt-get -y install build-essential

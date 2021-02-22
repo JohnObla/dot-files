bf69336d0c848d2e5c071bdfdc3865127cb78ef8
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-22 18:32:06 +0000

    install tmux

--- .custom-scripts/nvim-tmux-install.sh
+++ .custom-scripts/nvim-tmux-install.sh
@@ -7,3 +7,6 @@ sudo apt install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# install tmux
+sudo apt install tmux
+

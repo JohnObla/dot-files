9bc7d92c423d221f5eaa5b6f511601da55498ac7
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-24 15:50:50 +0000

    auto install neovim

--- .custom-scripts/nvim-tmux-install.sh
+++ .custom-scripts/nvim-tmux-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install neovim
-sudo apt install neovim
+sudo apt-get install -y neovim
 
 # install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \

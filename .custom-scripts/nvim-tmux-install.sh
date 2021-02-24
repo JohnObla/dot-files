655157d6f2308b51980b71233e1611312f7140af
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-24 12:49:05 +0000

    intall tpm

--- .custom-scripts/nvim-tmux-install.sh
+++ .custom-scripts/nvim-tmux-install.sh
@@ -10,3 +10,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 # install tmux
 sudo apt install tmux
 
+# install tpm
+[ ! -d "~/.tmux/plugins/tpm" ] \
+    && git clone https://github.com/tmux-plugins/tpm \
+    "~/.tmux/plugins/tpm"

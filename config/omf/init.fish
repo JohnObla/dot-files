70afaa3d641641bff1b135111c1c2ac4a10610cb
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-24 17:27:24 +0000

    add folder search source

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -13,5 +13,6 @@ alias oldvim="command vim"
 # allows hidden files, ignores .git
 set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --glob '!.git'"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+# source: https://github.com/BurntSushi/ripgrep/issues/169#issuecomment-253479209
 set -gx FZF_ALT_C_COMMAND "rg --hidden --files --null --glob '!.git' | xargs -0 dirname | uniq"
 

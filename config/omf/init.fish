7b401431ebe367fc2a3c9339b51d2cf27960ff16
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-24 17:26:27 +0000

    fzf alt c finds hidden folders

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -13,4 +13,5 @@ alias oldvim="command vim"
 # allows hidden files, ignores .git
 set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --glob '!.git'"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND "rg --hidden --files --null --glob '!.git' | xargs -0 dirname | uniq"
 

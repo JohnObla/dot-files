5ade55af22d2f63252fe879b0758c0f66cb86dd0
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-25 11:59:01 +0000

    install yarn

--- .custom-scripts/node-yarn-install.sh
+++ .custom-scripts/node-yarn-install.sh
@@ -4,6 +4,12 @@
 # https://stackoverflow.com/questions/11934997/how-to-install-make-in-ubuntu
 sudo apt-get install -y build-essential
 
-# install the n installer
+# install the n installer and node
 curl -L https://git.io/n-install | bash -s -- -y
+
+# install all packages auto complete
+npm install --global all-the-package-names
+
+# install yarn
+npm install --global yarn
 

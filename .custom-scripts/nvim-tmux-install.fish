fbcfa0e2d97939de28a7db6737bfbb32e1fac957
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-02-25 15:11:33 +0000

    install tmux plugins through command line

--- .custom-scripts/nvim-tmux-install.fish
+++ .custom-scripts/nvim-tmux-install.fish
@@ -9,17 +9,20 @@ chmod u+x ~/.bin/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# install plugins
+# install nvim plugins through command line
 # https://github.com/junegunn/vim-plug/issues/225#issuecomment-583541912
 nvim --headless +PlugInstall +qall
 
 # install tmux
 sudo apt-get install -y tmux
 
-# install tpm
+# install tmux plugin manager
 [ ! -d "$HOME/.tmux/plugins/tpm" ] \
     && git clone https://github.com/tmux-plugins/tpm \
     "$HOME/.tmux/plugins/tpm"
+
+# install tmux plugins through command line
+$HOME/.tmux/plugins/tpm/bin/install_plugins
 
 # install fzf
 sudo apt-get install -y fzf
